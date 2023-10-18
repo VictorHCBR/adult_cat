e153041089803d8007d0368e9166dea97471f50c
CodingShambles <victortheveloper@gmail.com>
2023-10-18 12:51:27 -0300

    Grown Cat func

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/VictorHCBR/adult_cat
+
+go 1.21.3
---
+++ go_get_2/grown_cat.go
@@ -0,0 +1,9 @@
+package grown_cat
+
+import (
+	"strings"
+)
+
+func GrownCat(noise string) string {
+	return "A grown cat makes a sound like " + strings.ToUpper(noise)
+}
